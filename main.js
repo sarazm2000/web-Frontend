d163b98f158925d0238af203f213f9dace7a7bea
sarazm2000 <sarazm.2000@gmail.com>
2021-12-01 19:37:05 +0330

    random header theme just added

--- main.js
+++ main.js
@@ -9,5 +9,19 @@ function changeMode() {
 
 function changeHeaderTheme (){
     var gradient = document.getElementById("header");
+    // gradient.classList.toggle("change-gradient-header");
+    var num1 = generateRandomHex();
+    var num2 = generateRandomHex();
+    // gradient.style.backgroundImage = "linear-gradient(num1, num2)";
+    document.documentElement.style.setProperty(`--gradient1`, `#${num1}`);
+    document.documentElement.style.setProperty(`--gradient2`, `#${num2}`);
     gradient.classList.toggle("change-gradient-header");
+}
+
+generateRandomHex = () => {
+    let number = '';
+    for (let i = 0; i < 6; ++i) {
+        number += (Math.floor(Math.random() * 16)).toString(16);
+    }
+    return number;
 }

18ef756b7da3e5290c811a61f81831c621ff22b9
sarazm2000 <sarazm.2000@gmail.com>
2021-12-01 18:27:05 +0330

    add function to change mode

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-function myFunction() {
-    // var element = document.body;
-    // element.classList.toggle("dark-mode-body");
+function changeMode() {
+    var bodyContainer = document.body;
+    bodyContainer.classList.toggle("dark-mode-body");
+    var waveContainer = document.getElementById("wave");
+    waveContainer.classList.toggle("dark-mode-wave");
+    var navbarContainer = document.getElementById("navbarContainer");
+    navbarContainer.classList.toggle("dark-mode-navbar")
+}
 
+function changeHeaderTheme (){
     
-
 }

f2776e62f416ff98eef81bf12f815641ca4d2f3f
sarazm2000 <sarazm.2000@gmail.com>
2021-12-01 20:36:33 +0330

    update

--- main.js
+++ main.js
@@ -4,15 +4,13 @@ function changeMode() {
     var waveContainer = document.getElementById("wave");
     waveContainer.classList.toggle("dark-mode-wave");
     var navbarContainer = document.getElementById("navbarContainer");
-    navbarContainer.classList.toggle("dark-mode-navbar")
+    navbarContainer.classList.toggle("dark-mode-navbar");
 }
 
 function changeHeaderTheme (){
     var gradient = document.getElementById("header");
-    // gradient.classList.toggle("change-gradient-header");
     var num1 = generateRandomHex();
     var num2 = generateRandomHex();
-    // gradient.style.backgroundImage = "linear-gradient(num1, num2)";
     document.documentElement.style.setProperty(`--gradient1`, `#${num1}`);
     document.documentElement.style.setProperty(`--gradient2`, `#${num2}`);
     gradient.classList.toggle("change-gradient-header");
